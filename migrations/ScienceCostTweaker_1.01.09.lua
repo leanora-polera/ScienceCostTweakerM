0d9c0a0509516ee09fb9d2e5708787a2af0ae562
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-12-12 20:22:25 +1300

    Tabs to spaces

--- migrations/ScienceCostTweaker_1.01.09.lua
+++ migrations/ScienceCostTweaker_1.01.09.lua
@@ -2,8 +2,7 @@ local sctmlibrt = require("sctmlibrt")
 
 for _, force in pairs(game.forces) do
   if force.technologies['advanced-material-processing-2'].researched then
-		sctmlibrt.enable_recipe(force.recipes, "sct-prod-biosilicate")
-		sctmlibrt.enable_recipe(force.recipes, "sct-prod-baked-biopaste")
+    sctmlibrt.enable_recipe(force.recipes, "sct-prod-biosilicate")
+    sctmlibrt.enable_recipe(force.recipes, "sct-prod-baked-biopaste")
   end
 end
-
